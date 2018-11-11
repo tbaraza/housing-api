f4c95ae30230038138f7a17e0c948b635461ba26
tbaraza <tonida.baraza@andela.com>
2018-11-11 17:03:57 +0300

    Set up eslint and nodemon

---
+++ server.js
@@ -0,0 +1,32 @@
+const Hapi = require('hapi');
+
+const server = Hapi.server({
+  host: 'localhost',
+  port: 9000
+});
+
+// Add the route
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: (request, h) => 'hello world today'
+});
+
+// Start the server
+const start = async () => {
+  await server.register({
+    plugin: require('hapi-pino'),
+    options: {
+      prettyPrint: true,
+      logEvents: ['response', 'onPostStart']
+    }
+  });
+  await server.start();
+  console.log('Server running at:', server.info.uri);
+};
+
+process.on('unhandledRejection', err => {
+  console.error(err);
+});
+
+start();

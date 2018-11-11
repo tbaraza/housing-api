f6fd34443b09a36420be623da2342c40dece5879
tbaraza <tonida.baraza@andela.com>
2018-11-11 16:03:18 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const Hapi = require("hapi");
+
+const server = Hapi.server({
+  host: "localhost",
+  port: 9000
+});
+
+// Add the route
+server.route({
+  method: "GET",
+  path: "/",
+  handler: (request, h) => "hello world today"
+});
+
+// Start the server
+const start = async () => {
+  try {
+    await server.start();
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  console.log("Server running at:", server.info.uri);
+};
+
+start();
